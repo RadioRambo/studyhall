c775c3d8104f2766527b96b9107a5c6e0ac1b363
RadioRambo <radio@rambo>
2023-09-02 11:34:11 +0530

    Add line between forms

--- src/routes/hometuitions/+page.svelte
+++ src/routes/hometuitions/+page.svelte
@@ -10,8 +10,19 @@
   line="Experienced tutors with great teaching skills without leaving your comfort of time and convenience of home."
 />
 <Heading heading="FILL THE FORM" />
-<div class="flex flex-col xl:flex-row gap-16">
+<div class="flex flex-col xl:flex-row gap-16 xl:gap-0 xl:justify-between">
   <ParentsForm />
+  <div class="hidden xl:block mt-12">
+    <svg
+      width="1"
+      height="600"
+      viewBox="0 0 1 450"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="0.5" y1="600" x2="0.5" stroke="black" />
+    </svg>
+  </div>
 
   <TutorsForm />
 </div>
--- src/routes/hometuitions/TutorsForm.svelte
+++ src/routes/hometuitions/TutorsForm.svelte
@@ -1,7 +1,7 @@
 <div class="mx-6">
   <h1 class="font-semibold text-3xl mt-10">For Tutors</h1>
   <h2 class="mb-10">If you are looking for a part time job</h2>
-  <form class="mx-0 md:mx-6" action="submit_form.php" method="POST">
+  <form class="mx-0 md:ml-6" action="submit_form.php" method="POST">
     <input
       class="bg-secondarylight outline outline-2 outline-outlinelight dark:outline-outlinedark rounded-3xl placeholder:text-black placeholder:font-medium placeholder:text-xl px-6 py-2 mb-4 w-[55vw] max-w-[300px] lg:max-w-none lg:w-80"
       placeholder="Name"
